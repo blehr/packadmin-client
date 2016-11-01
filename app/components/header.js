7cc3ca2b88c9fe66282937ea1d79e9f0418e6018
Brandon Lehr <blehr.mail@gmail.com>
2016-10-31 22:54:01 -0400

    styles

--- app/components/header.js
+++ app/components/header.js
@@ -2,15 +2,25 @@ import React from 'react';
 import MyAppBar from './my_app_bar';
 import UserDisplay from '../containers/user_display';
 
+import logoUrl from '../images/pack_admin_logo.png';
+
 const Header = () => (
   <header className="container">
     <div className="row text-center" >
       <div className="col-sm-12">
         <div className="header">
-          <h1 className="headerFont">Pack Admin</h1>
+          <div>
+            <h1>The Pack Admin</h1>
+            <h3>The Cub Master's Secret</h3>
+          </div>
+          {/* <div className="logo-img">
+            <img src={logoUrl} alt="pack admin logo" className="img-responsive" />
+          </div> */}
         </div>
-        <MyAppBar />
-        <UserDisplay />
+        <div className="app-bar-container">
+          <MyAppBar />
+          <UserDisplay />
+        </div>
       </div>
     </div>
   </header>
